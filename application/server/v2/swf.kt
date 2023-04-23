5a16304efcac32a089673f6f72f89d819f881609
ゼーレ <131597070+seele-off@users.noreply.github.com>
2023-04-23 19:54:57 +0600

    Update swf.kt

--- application/server/v2/swf.kt
+++ application/server/v2/swf.kt
@@ -1,1 +1,7 @@
-[{"img":"https://i.pinimg.com/736x/bc/d7/eb/bcd7ebd6d39813f9cf3d14f584aaecd5.jpg","title":"WaiFu","api":"https://api.waifu.pics/sfw/waifu"}]
+[ {
+"img":"https://i.pinimg.com/736x/bc/d7/eb/bcd7ebd6d39813f9cf3d14f584aaecd5.jpg",
+"title":"WaiFu",
+"api":"https://api.waifu.pics/sfw/waifu",
+"type":"SFW",
+"category":"WAIFU"
+} ]
