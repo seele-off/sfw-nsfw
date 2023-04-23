c3fca59a0a4cc317bb136eb1e3926d7c4bbc46e7
ゼーレ <131597070+seele-off@users.noreply.github.com>
2023-04-23 22:09:36 +0600

    Update swf.kt

--- application/server/v2/swf.kt
+++ application/server/v2/swf.kt
@@ -10,4 +10,22 @@
 "api":"https://api.waifu.pics/sfw/neko",
 "type":"SFW",
 "category":"NEKO"
+},
+{"img":"https://i.pinimg.com/originals/eb/82/a4/eb82a40f978f8eeb3bdb7b4c191e848c.jpg",
+"title":"Shinobu",
+"api":"https://api.waifu.pics/sfw/shinobu",
+"type":"SFW",
+"category":"SHINOBU"
+},
+{"img":"https://64.media.tumblr.com/f3d0e2e7ea36d2d94934b97b0cf6fd28/456f1fcd862250cb-e6/s1280x1920/2713d465a51c4831d9a79b54ac592caa884478b7.jpg",
+"title":"Megumin",
+"api":"https://api.waifu.pics/sfw/megumim",
+"type":"SFW",
+"category":"MEGUMIN"
+},
+{"img":"https://i.pinimg.com/736x/25/dd/6d/25dd6d6f19fda46174cff702d17bd351.jpg",
+"title":"Bully",
+"api":"https://api.waifu.pics/sfw/bully",
+"type":"SFW",
+"category":"BULLY"
 } ]
