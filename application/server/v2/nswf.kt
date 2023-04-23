86817f16ab4bdd205fc8f9a44ce7ecbaf12f43aa
ゼーレ <131597070+seele-off@users.noreply.github.com>
2023-04-23 20:21:04 +0600

    Create nswf.kt

---
+++ application/server/v2/nswf.kt
@@ -0,0 +1,7 @@
+[ {
+"img":"https://i.pinimg.com/736x/96/9e/e5/969ee563b5d5f12cb5f46d4b16b222a4.jpg",
+"title":"WaiFu",
+"api":"https://api.waifu.pics/nsfw/waifu",
+"type":"NSFW",
+"category":"WAIFU"
+} ]
